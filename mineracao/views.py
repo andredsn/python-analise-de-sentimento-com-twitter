522f8b775f67c2b45f1fc6756fdbf31ce21d185f
Andre Nascimento <andresoteropolitano10@gmail.com>
2018-03-29 13:55:14 -0300

    inserindo a chamada do main() e o método calcularClasse para exibir os totais

--- mineracao/views.py
+++ mineracao/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from mineracao.modulos.analise_sentimento import getDataFrame, calcularPercentualTotal,\
-    calcularClasses
+
+from mineracao.modulos.analise_sentimento import getDataFrame, calcularPercentualTotal, calcularClasses, main
+
 
 def index(request):
+    main()
     df =getDataFrame()
     percentuais=calcularPercentualTotal(df)
     totais=calcularClasses(df)
     total=len(df)
-    return render(request, 'mineracao/index.html', {"total": total, "percentualSeguros": percentuais[0], "percentualInseguros": percentuais[1], "percentualNeutros": percentuais[2], "totalSeguros": totais[0], "totalInseguros": totais[1], "totalNeutros": totais[2]})
+    
+    texto=df['texto'].tail(1).values
+    sentimento=df['sentimento'].tail(1).values
+    
+    return render(request, 'mineracao/index.html', {"total": total, "percentualSeguros": percentuais[0], "percentualInseguros": percentuais[1], "percentualNeutros": percentuais[2], "totalSeguros": totais[0], "totalInseguros": totais[1], "totalNeutros": totais[2], "texto": texto[0], "sentimento": sentimento[0]})
