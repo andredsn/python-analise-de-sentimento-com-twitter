5218186ae716f921be5a39d22b56ef6a2b9952e9
Andre Nascimento <andresoteropolitano10@gmail.com>
2018-07-01 22:49:41 -0300

    mudanças em view.py

--- mineracao/views.py
+++ mineracao/views.py
@@ -4,8 +4,10 @@ from mineracao.modulos.analise_sentimento import getDataFrame, calcularPercentua
 
 
 def index(request):
+    
     main()
     df =getDataFrame()
+    df=df.head(3000)
     percentuais=calcularPercentualTotal(df)
     totais=calcularClasses(df)
     total=len(df)
@@ -13,5 +15,5 @@ def index(request):
     texto=df['texto'].tail(1).values
     sentimento=df['sentimento'].tail(1).values
     
-    return render(request, 'mineracao/index.html', {"total": total, "percentualSeguros": percentuais[0], "percentualInseguros": percentuais[1], "percentualNeutros": percentuais[2], "totalSeguros": totais[0], "totalInseguros": totais[1], "totalNeutros": totais[2], "texto": texto[0], "sentimento": sentimento[0]})
+    return render(request, 'mineracao/index.html', {"total": total, "percentualInseguros": percentuais[0], "percentualOutros": percentuais[1], "totalInseguros": totais[0], "totalOutros": totais[1], "texto": texto[0], "sentimento": sentimento[0]})
 
